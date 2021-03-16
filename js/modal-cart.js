97e3addab24ee54c95c672e7960a854754d12409
Anaena <carrie@mail.ru>
2021-03-16 19:54:28 +0300

    Modal-cart отформатирован, slide, map и modal-form удалены

--- js/modal-cart.js
+++ js/modal-cart.js
@@ -1,23 +1,23 @@
 (function () {
-const modalCart = document.querySelector('.modal-cart');
-const openPopupButtons = document.querySelectorAll('.btn-buy');
-const closePopupButton = modalCart.querySelector('.modal-close');
+  const modalCart = document.querySelector('.modal-cart');
+  const openPopupButtons = document.querySelectorAll('.btn-buy');
+  const closePopupButton = modalCart.querySelector('.modal-close');
 
-openPopupButtons.forEach(btn =>
-  btn.addEventListener('click', function () {
-    modalCart.classList.add('modal--show');
-  })
-);
+  openPopupButtons.forEach(btn =>
+    btn.addEventListener('click', function () {
+      modalCart.classList.add('modal--show');
+    })
+  );
 
-closePopupButton.addEventListener('click', function () {
-  modalCart.classList.remove('modal--show');
-});
+  closePopupButton.addEventListener('click', function () {
+    modalCart.classList.remove('modal--show');
+  });
 
-const newLocal = document.addEventListener;
-newLocal('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-  modalCart.classList.remove('modal--show');
-  }
-});
+  const newLocal = document.addEventListener;
+  newLocal('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      modalCart.classList.remove('modal--show');
+    }
+  });
 }());
 
